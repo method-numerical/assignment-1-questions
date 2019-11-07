bdb51d900c8c340d02684a6770456f92e0a6143c
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-11-07 20:35:27 +0530

    using in statement instead....

--- pow2.py
+++ pow2.py
@@ -1,12 +1,11 @@
 """
 eliminating found and if statement.
-using if else instead.
+using in instead.
 """
 L=[1,2,4,8,16,32,64]
 X=5
-i=0
-for i in range(6):
-    if L[i]==2**X:
-        print('found at',i,'.')
-    else:
-        i=i+1
+
+if 2**X in L:
+    print('found at',L.index(2**X),'.')
+else:
+    print('not found..')
