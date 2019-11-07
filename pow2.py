b21319eb84b0a6d85f02ca52ff3771dee7184d7a
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-11-07 20:33:13 +0530

    using if else statement....

--- pow2.py
+++ pow2.py
@@ -1,11 +1,12 @@
 """
 eliminating found and if statement.
-using while else instead.
+using if else instead.
 """
 L=[1,2,4,8,16,32,64]
 X=5
 i=0
-while not(2**X==L[i]):
-    i=i+1
-else:
-    print('found at',i,'.')
+for i in range(6):
+    if L[i]==2**X:
+        print('found at',i,'.')
+    else:
+        i=i+1
