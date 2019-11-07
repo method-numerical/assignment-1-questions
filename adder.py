39c4250b5870e4ca353b920daa5b91d147c19ded
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-11-07 18:31:14 +0530

    generalising the adder function to add any arbitrary numbers.

--- adder.py
+++ adder.py
@@ -1,11 +1,9 @@
 """
-addition of list, string, floating points.
+generalising adder function to add any arbitrary numbers.
 
 """
-def adder(x,y):
-    z=x+y
-    return z
-
-print(adder('s','t'))
-print(adder([4,0],[9,8]))
-print(adder(34.07,78.09))
+def adder(num):
+    sum=0
+    for j in num:
+        sum=sum+j
+    return sum
