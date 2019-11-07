f3890bca44bb988390fb8594e5618e52a2763d1c
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-11-07 18:38:27 +0530

    adder function taking keywords good,bad,ugly.

--- adder.py
+++ adder.py
@@ -1,9 +1,14 @@
 """
-generalising adder function to add any arbitrary numbers.
+adder function taking only good, bad, ugly kewwords.
 
 """
-def adder(num):
-    sum=0
-    for j in num:
-        sum=sum+j
-    return sum
+def adder(good,bad,ugly):
+    z=good+bad+ugly
+    return z
+good=1; bad=2; ugly=3;    #default values....
+
+a=adder(good=5,ugly=1)
+
+#after executing above code error came as TypeError: adder() missing 1 required positional argument: 'bad'.
+
+#It implies that this function must take all key words good,bad,ugly.
