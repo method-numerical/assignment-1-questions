1b62139422dd18b578d85b46acfe6f6b944835fd
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-11-07 20:42:46 +0530

    improving code, taking 2**X expressions out of loop ....

--- pow2.py
+++ pow2.py
@@ -1,8 +1,14 @@
 """
-code to generate some pow of 2.
-instead of hard coding....
+improving the previous code.
+moving 2**X expression out of the loop....
 """
 L=[]
+X=5
 for i in range(6):
     L.append(2**i)
-print(L)
+a=L.index(2**X)     #now out of loop
+
+if a>-1:
+    print('found at',a,'.')
+else:
+    print('not found....')
