53199ad8880cf93650e248396af91a9dce5b394a
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-11-07 20:38:01 +0530

    generate list of pow of 2 instead of hard coding.

--- pow2.py
+++ pow2.py
@@ -1,11 +1,8 @@
 """
-eliminating found and if statement.
-using in instead.
+code to generate some pow of 2.
+instead of hard coding....
 """
-L=[1,2,4,8,16,32,64]
-X=5
-
-if 2**X in L:
-    print('found at',L.index(2**X),'.')
-else:
-    print('not found..')
+L=[]
+for i in range(6):
+    L.append(2**i)
+print(L)
